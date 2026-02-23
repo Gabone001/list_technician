5002c9f8fe2080f7e22399312c56f1b38b6deb42
Gabriel Mihail <gabymihail@yahoo.com>
2026-02-23 15:07:21 +0000

    Average, maximum and minimum years of experience.

--- List technicians/list_technician_extra_ex.py
+++ List technicians/list_technician_extra_ex.py
@@ -22,9 +22,10 @@ for name, role, years in zip(names, roles, years_experience):
   print(f"{name:<20} | {role:<28} | {years:>5}")
 print("\nTotal number: ", len(names))
 
-# # >>>>>>>>>>>>>>>>>>
-# # ADD new technician
-# # >>>>>>>>>>>>>>>>>>
+# # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# # Exercise 2- ADD new technician
+# # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 # name = input("Enter a new person: ").strip()
 # role = input("Enter a title: ").strip()
 # years = input("Enter the years of experience: ").strip()
@@ -41,9 +42,10 @@ print("\nTotal number: ", len(names))
 #         print("New person: ", name,"-" "Role: ", role,"-" "Years Experience: ", years)
 #         print("Book added successfully.")
 
-# >>>>>>>>>>>>>>>>>>
-# Delete a technician
-# >>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Exercise 3- Delete a technician
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 # name = input("Enter a person that left: ").strip()
 #
 # if name not in  names:
@@ -58,44 +60,51 @@ print("\nTotal number: ", len(names))
 #             file.write(f" {name}, {role}, {years} \n")
 #     print("Book deleted successfully. ")
 
-# # >>>>>>>>>>>>>>>>>>
-# # Update technician's details
-# # >>>>>>>>>>>>>>>>>>
+# # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# # Exercise 4- Update technician's details or years of experience
+# # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# name = input("\nEnter technician's name: ").strip()
+#
+# if name not in names:
+#     print("That name is not in the database.")
+# else:
+#     index = names.index(name)   # <--- IMPORTANT
+#
+#     print("\nWhat would you like to update?")
+#     print("1. Job Role")
+#     print("2. Years of Experience")
+#     choice = input("Choose (1/2): ")
+#
+#     if choice == "1":
+#         new_role = input("Enter new job role: ")
+#         roles[index] = new_role
+#
+#     elif choice == "2":
+#         new_years = int(input("Enter new years of experience: "))
+#         years_experience[index] = new_years
+#
+#     else:
+#         print("Invalid option!")
+#
+#     # Save all lists back to file
+#     with open("staff.txt", "w") as file:
+#         for n, r, y in zip(names, roles, years_experience):
+#             file.write(f"{n},{r},{y}\n")
+#
+#     # Show updated record
+#     print("\nUpdated Technician Record:")
+#     print(f"Name: {names[index]}")
+#     print(f"Role: {roles[index]}")
+#     print(f"Experience: {years_experience[index]} years")
 
 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Exercise 5- Average, the highest and the lowest years of experience.
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-name = input("\nEnter technician's name: ").strip()
-
-if name not in names:
-    print("That name is not in the database.")
-else:
-    index = names.index(name)   # <--- IMPORTANT
-
-    print("\nWhat would you like to update?")
-    print("1. Job Role")
-    print("2. Years of Experience")
-    choice = input("Choose (1/2): ")
-
-    if choice == "1":
-        new_role = input("Enter new job role: ")
-        roles[index] = new_role
-
-    elif choice == "2":
-        new_years = int(input("Enter new years of experience: "))
-        years_experience[index] = new_years
-
-    else:
-        print("Invalid option!")
-
-    # Save all lists back to file
-    with open("staff.txt", "w") as file:
-        for n, r, y in zip(names, roles, years_experience):
-            file.write(f"{n},{r},{y}\n")
-
-    # Show updated record
-    print("\nUpdated Technician Record:")
-    print(f"Name: {names[index]}")
-    print(f"Role: {roles[index]}")
-    print(f"Experience: {years_experience[index]} years")
-
+average = sum(years_experience) / len(years_experience)
+print(f"Average years experience: {average}")
+print(max(years_experience))
+print(min(years_experience))
 
